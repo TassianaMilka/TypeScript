b83cda1819fdf6d737e94da14465ce95e492dd27
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-07 11:46:29 -0300

    Update Basicexamplestring.ts

--- TypeScript/Basic/Basicexamplestring.ts
+++ TypeScript/Basic/Basicexamplestring.ts
@@ -1,4 +1,26 @@
-//Create  about example basic about print one string 
+//Create  about example basic about print one string with var,let and const
+
+
+
+
+
+
+//String variable
+var message:string = "Example one about basic TypeScript!";
+
+//Print about  message 
+console.log(message);
+
+
+
+//String variable
+let message:string = "Example one about basic TypeScript!";
+
+//Print about  message 
+console.log(message);
+
+
+
 
 //String variable
 const message:string = "Example one about basic TypeScript!";
