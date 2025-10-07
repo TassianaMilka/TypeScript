d85c6570064fb1f7114bfe2290ca4a16a2a1b8f5
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-07 11:43:50 -0300

    Create Basicexamplestring.ts

---
+++ TypeScript/Basic/Basicexamplestring.ts
@@ -0,0 +1,7 @@
+//Create  about example basic about print one string 
+
+//String variable
+const message:string = "Example one about basic TypeScript!";
+
+//Print about  message 
+console.log(message);
