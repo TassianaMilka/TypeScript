3a78c116d372e7e69e7928e123b0884d404e58f6
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-07 11:54:20 -0300

    Create Basicexamplenumberadd.ts

---
+++ TypeScript/Basic/Basicexamplenumberadd.ts
@@ -0,0 +1,27 @@
+
+
+//Create  about example basic about number add(+) and print result.
+
+
+
+//Number variable and add
+var num: number = 80+80;
+
+//Print number
+console.log(num);
+
+
+
+//Number variable and add
+let num: number = 80+80;
+
+//Print number
+console.log(num);
+
+
+
+//Number variable and add
+const num: number = 80+80;
+
+//Print number
+console.log(num);
