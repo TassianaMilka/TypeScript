9998979fc9145bb1c08f2562eeacc97873548dd4
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-07 13:16:54 -0300

    Create Basicexamplestringarray.go

---
+++ TypeScript/Basic/Basicexamplestringarray.go
@@ -0,0 +1,38 @@
+
+//Create code aobut array with number and string
+
+
+
+//Variable number and string array
+var num: Array<number> = [120, 220, 320]; 
+
+var text: Array<string> = ["Exemplo String Array"]; 
+
+//Print Arrays
+console.log(num); 
+
+console.log(text); 
+
+
+
+//Variable number and string array
+let num: Array<number> = [120, 220, 320]; 
+
+let text: Array<string> = ["Exemplo String Array"]; 
+
+//Print Arrays
+console.log(num); 
+
+console.log(text); 
+
+
+
+//Variable number and string array
+const num: Array<number> = [120, 220, 320]; 
+
+const text: Array<string> = ["Exemplo String Array"]; 
+
+//Print Arrays
+console.log(num); 
+
+console.log(text); 
