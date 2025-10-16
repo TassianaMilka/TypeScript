8d63c0aef1858a6fdb9a663ab3583e97e597534c
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-16 01:54:49 -0300

    Fix tuple value from 900 to 90

--- TypeScript/Basic/Basicexampletuple/Basicexampletuple.ts
+++ TypeScript/Basic/Basicexampletuple/Basicexampletuple.ts
@@ -1,7 +1,7 @@
 //Create one tuple with string name/money.
 
 //Variable about tuple
-let list: [string, number] = ['Table and chair U$',900]; 
+let list: [string, number] = ['Table and chair U$',90]; 
 
 //Print 
 console.log(list[0],list[1]); 
