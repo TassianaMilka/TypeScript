9edad728183a97751ec731982294f671709a7cac
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-21 14:58:27 -0300

    Add if/else example for name validation
    Implement basic if/else structure to check name.

---
+++ TypeScript/Basic/Basicexampleifandelse/Basicexampleifandelse.go
@@ -0,0 +1,24 @@
+//Crete one code about name and use if/else.
+
+
+//Variable
+
+let nome="Tassiana Milka!";
+
+
+//structure
+
+if (name=="Tassiana Milka"){ 
+  
+  //Print message
+console.log("Name is true!"); 
+
+
+}else if(name="Milka"){
+  
+  //Print message
+  console.log("Return!");
+  
+
+
+}
