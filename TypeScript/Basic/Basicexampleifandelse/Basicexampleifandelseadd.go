3147857de61aa1e9d65f3c7025d27de2552be46b
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-21 20:05:15 -0300

    Create Basicexampleifandelseadd.go

---
+++ TypeScript/Basic/Basicexampleifandelse/Basicexampleifandelseadd.go
@@ -0,0 +1,33 @@
+
+//Crete add in code about  user website Tassiana U$3400 with U$0.9 use if/else and print result.
+
+
+//Variable
+let name="Tassiana"
+var U$=3400;
+
+
+//structure
+
+if (U$>1200){ 
+  
+  //Print message
+  console.log("Tassiana");
+  console.log("Money U$",U$); 
+  var add=3400+0.9;
+  console.log(add);
+  
+
+}else if(U$<1000){
+  
+  //Print message
+  console.log("Ok!");
+  
+
+}else{
+  
+  
+  //Print message
+  console.log("Return!");
+  
+}
