c7dee89c318e2c97425e18187c0d77c5c5caf2b0
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-21 18:56:56 -0300

    Add if/else structure for U$3400 variable

---
+++ TypeScript/Basic/Basicexampleifandelse/BasicexampleifandelseU$.go
@@ -0,0 +1,29 @@
+//Crete  code about U$3400 and use if/else.
+
+
+//Variable
+
+var U$=3400;
+
+
+//structure
+
+if (U$>1200){ 
+  
+  //Print message
+  console.log("Money U$",U$); 
+  
+
+}else if(U$<1000){
+  
+  //Print message
+  console.log("Ok!");
+  
+
+}else{
+  
+  
+  //Print message
+  console.log("Return!");
+  
+}
