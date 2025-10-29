380d19a32c23743627670e9ecaf3e4fa5fa6ea60
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-28 22:23:30 -0300

    Create Basicexampleifandelse&&.go

---
+++ TypeScript/Basic/Basicexampleifandelse/Basicexampleifandelse&&.go
@@ -0,0 +1,22 @@
+//Create r$900 with if and else(&&) message  and message return error.
+
+//Variable r$ 
+const r$ = 900;
+
+
+
+//structure
+
+if( r$>=800 &&  r$<=900){
+
+//Print
+console.log("Ok,return!");
+
+
+}
+
+else{
+
+    //print
+    console.log("Return error!");
+}
