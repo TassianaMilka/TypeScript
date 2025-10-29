8a8b654eb1ebf15f3e87be4c559c72ea8f8a063a
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-28 22:22:28 -0300

    Create Basicexampleifandelse||.go

---
+++ TypeScript/Basic/Basicexampleifandelse/Basicexampleifandelse||.go
@@ -0,0 +1,21 @@
+//Create name with if and else(||) message  and message return name error.
+
+//Variable string name
+const name = "Mark";
+
+
+//structure
+
+if(name=="Milka" ||  name=="R$678.00"){
+
+
+console.log("Yes return name!");
+
+
+}
+
+else{
+
+
+    console.log("Return name error!");
+}
