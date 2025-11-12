fb0a4adf35f80a88261e665aff7cd686cc05ce77
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-11-12 00:21:18 -0300

    Create Basicexampleloopfor.ts

---
+++ TypeScript/Basic/Basicexampleloopfor/Basicexampleloopfor.ts
@@ -0,0 +1,11 @@
+// Create code names with loop string names.
+
+//Variable string array 
+var names = ["Tassiana", "Milka"]; 
+
+//Loop
+for (let i = 0; i < names.length; i++) { 
+  
+//Print names
+console.log(names[i]); 
+} 
